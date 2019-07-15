2f42972a9a6aff054d79834fcf1bafbdc740fe04
unveilrock <52118387+unveilrock@users.noreply.github.com>
2019-07-14 21:20:03 -0400

    Create best-time-to-buy-and-sell-stock.py

---
+++ best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,13 @@
+#dp
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        maximum = 0
+        minimum = prices[0]
+        for i in range(len(prices)):
+            if prices[i]< minimum:
+                minimum = prices[i]
+            elif prices[i]-minimum > maximum:
+                maximum = prices[i]-minimum
+        return maximum
